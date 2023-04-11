c502bcef7ea4dcd493540c0328560481e4f45fea
Fauzul <fauzularianto@students.amikom.ac.id>
2023-04-11 20:47:27 +0700

    first commit

---
+++ Dosen.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramDosen
+{
+    class Dosen
+    {
+        private int id;
+        private string nama;
+        private int nik;
+        private string gender;
+        private string course;
+
+        public Dosen()
+        {
+            this.id = 1;
+            this.nama = "Dosen Pengampu";
+            this.nik = 1234567890;
+            this.gender = "male";
+            this.course = "Pemrograman Berorientasi Objek";
+        }
+        public void display()
+        {
+            Console.WriteLine("ID: " + this.id);
+            Console.WriteLine("Nama: " + this.nama);
+            Console.WriteLine("NIK: " + this.nik);
+            Console.WriteLine("Gender: " + this.gender);
+            Console.WriteLine("Course: " + this.course);
+        }
+
+        public void attDosen(string nama, int nik)
+        {
+            this.nama = nama;
+            this.nik = nik;
+        }
+
+        public void addCourse(string course)
+        {
+            this.course += ", " + course;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramDosen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dosen dosen = new Dosen();
+
+            Console.WriteLine($"Default Dosen Pengampu");
+            dosen.display();
+
+            Console.WriteLine("Masukkan Nama dan NIK Dosen Baru");
+            dosen.attDosen(Console.ReadLine(), Convert.ToInt32(Console.ReadLine()));
+            dosen.display();
+
+            Console.WriteLine("Masukkan Mata Kuliah Baru");
+            dosen.addCourse(Console.ReadLine());
+            dosen.display();
+
+            Console.ReadLine();
+        }
+    }
+}
